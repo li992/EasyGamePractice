03521cd63006ec0c4700922f915825fce7bcaded
Qiaoyi Li <QLI@eina.evrazinc.ad>
2018-09-17 12:25:36 -0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace EasyGamePractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PBShow = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PBShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PBShow
+            // 
+            this.PBShow.Location = new System.Drawing.Point(88, 53);
+            this.PBShow.Name = "PBShow";
+            this.PBShow.Size = new System.Drawing.Size(540, 340);
+            this.PBShow.TabIndex = 0;
+            this.PBShow.TabStop = false;
+            this.PBShow.Paint += new System.Windows.Forms.PaintEventHandler(this.PBShow_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 535);
+            this.Controls.Add(this.PBShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.PBShow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PBShow;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyGamePractice
+{
+    public partial class Form1 : Form
+    {
+        static int x=50, y=50;           // x,y are the coordinate; width and height are the rectangle properties -- global variables
+        private int width, height, WIDTH, HEIGHT;       // width,height are the distance to the edges;  WIDTH and HEIGHT are the actual box properties
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PBShow_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle r = new Rectangle(x, y, width, height);
+            Graphics g = e.Graphics;                                //g is the GDI, e is the Event
+            g.FillRectangle(new SolidBrush(Color.Black), r);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                MoveUp();
+            if (e.KeyCode == Keys.Down)
+                MoveDown();
+            if (e.KeyCode == Keys.Left)
+                MoveLeft();
+            if (e.KeyCode == Keys.Right)
+                MoveRight();
+        }
+
+        // Following four functions are the moving functions
+        private void MoveUp()
+        {
+            if (y < height)
+            {
+                return;
+            }
+            y = y - height;
+            this.PBShow.Refresh();
+        }
+
+        private void MoveDown()
+        {
+            if (y > HEIGHT - 2*height)
+            {
+                return;
+            }
+            y = y + height;
+            this.PBShow.Refresh();
+        }
+
+        private void MoveLeft()
+        {
+            if (x < width)
+            {
+                return;
+            }
+            x = x - width;
+            this.PBShow.Refresh();
+        }
+
+        private void MoveRight()
+        {
+            if (x > WIDTH - 2*width)
+            {
+                return;
+            }
+            x = x + width;
+            this.PBShow.Refresh();
+        }
+
+        // Setup the background box properties
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            width = 30;
+            height = 30;
+            HEIGHT = PBShow.Height;      // make them the same size as the box value
+            WIDTH = PBShow.Width;
+            PBShow.BackColor = Color.White;
+        }
+
+
+
+
+    }
+}
